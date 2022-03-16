3194790c651c973a3c41833c4042fa26810fe15e
Deepthi Konde <deepthi.kondae@gmail.com>
2022-03-16 13:05:10 +0000

    Technical Challenge UI

---
+++ config/constants.js
@@ -0,0 +1,3 @@
+export const BASE_IMAGE_URL = 'https://image.tmdb.org/t/p/w200'
+export const BASE_IMAGE_URL_BIG = 'https://image.tmdb.org/t/p/w500'
+export const NO_POSTER_IMG = '../assets/NO-POSTER.jpg'
